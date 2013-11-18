7e0cb3c539e322554006d0f3319d0ee56267d548
Michael Owings <mowings@turbosquid.com>
2013-11-18 10:16:59 -0600

    run intructions

--- host-setup.sh
+++ host-setup.sh
@@ -3,6 +3,7 @@
 # Sets up or updates an lxc-host
 # First run is slow -- updates are fast
 # The base distribution should be Ubuntu 13.04 (x64). If using a VM you should probably intall the VM tools
+# To run: wget -qO- https://raw.github.com/turbosquid/lxc-host-setup/master/host-setup.sh | bash
  
 set -iex
  
